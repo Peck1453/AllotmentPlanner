96eb297d95aa7d0c6e51aad4c64270a9137a77ef
Peck1453 <andy@the-pecks.co.uk>
2018-06-12 17:06:07 +0100

    First Controller and view

--- AllotmentPlanner/App_Start/RouteConfig.cs
+++ AllotmentPlanner/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AllotmentPlanner
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Crop", action = "GetCrops", id = UrlParameter.Optional }
             );
         }
     }
---
+++ AllotmentPlanner/Controllers/ApplicationController.cs
@@ -0,0 +1,21 @@
+﻿using AllotmentPlanner.Services.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllotmentPlanner.Controllers
+{
+    public abstract class ApplicationController : Controller
+    {
+        public CropService _cropService;
+
+        public ApplicationController()
+        {
+            _cropService = new CropService();
+
+
+        }
+    }
+}
