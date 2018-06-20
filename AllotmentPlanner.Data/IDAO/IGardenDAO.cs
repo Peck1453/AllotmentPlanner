6bf269c73bc4d4c4e004555af6a42de27d51e849
Peck1453 <andy@the-pecks.co.uk>
2018-06-20 15:32:15 +0100

    Created IGardenDAO

---
+++ AllotmentPlanner.Data/IDAO/IGardenDAO.cs
@@ -0,0 +1,21 @@
+﻿using AllotmentPlanner.Data.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllotmentPlanner.Data.IDAO
+{
+    public interface IGardenDAO
+    {
+        IList<AllotmentPlanner.Data.GardenLocation> GetGardenLocations();
+
+        Crop GetGardenLocation(int id);
+        void editCrop(GardenLocation gardenLocation);
+
+        void addCrop(GardenLocation gardenLocation);
+        void DeleteCrop(GardenLocation gardenLocation);
+
+    }
+}
